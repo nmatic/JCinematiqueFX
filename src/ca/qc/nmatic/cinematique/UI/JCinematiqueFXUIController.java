a6be6811100f471bfe99c42f2b4dc1758aaf3ec3
Victor B. <vicbab@yandex.com>
2017-04-09 14:26:03 -0400

    Minor formulas modifications

--- src/ca/qc/nmatic/cinematique/UI/JCinematiqueFXUIController.java
+++ src/ca/qc/nmatic/cinematique/UI/JCinematiqueFXUIController.java
@@ -111,12 +111,16 @@ public class JCinematiqueFXUIController implements Initializable {
         entryInitialVel.setText("");
         desiredValue.setValue("- SELECT -");
         desiredValue.setItems(chosenValue);
+        posGraph.getData().clear();
+        velGraph.getData().clear();
+        accGraph.getData().clear();
+        nbSeries = 0;
     }
 
     @FXML
     public void resetButton() {
         resetFields();
-        resetCharts();
+
     }
 
     @FXML
